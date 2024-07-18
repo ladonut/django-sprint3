19c151b8dcc020ba606e2b3c8bdb23f1bbdba3fb
Larisa Karteleva <karteleva.larisa@gmail.com>
2024-07-18 14:45:19 +0300

    edit admin zone

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -13,7 +13,25 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Category)
-admin.site.register(Location)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'slug', 
+                    'is_published', 'created_at')
+    list_display_links = ('title',)
+    list_editable = ('is_published',)
+    list_filter = ('id', )            
+    empty_value_display = '-пусто-'
+
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('id', 'name',
+                    'is_published', 'created_at')
+    list_display_links = ('name',)
+    list_editable = ('is_published',)
+    list_filter = ('name',)            
+    empty_value_display = 'Планета Земля'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(Post, PostAdmin)
 
